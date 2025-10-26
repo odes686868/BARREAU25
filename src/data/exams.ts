2ced8ed9ee2ebad1e54387aa8fea45a70035fb22
odes686868 <barreauia25@gmail.com>
2025-10-25 20:11:05 -0400

    Updated exams.ts

--- src/data/exams.ts
+++ src/data/exams.ts
@@ -6,14 +6,14 @@ export interface Exam {
 
 export const exams: Exam[] = [
   {
-    id: 1,
-    name: 'Éthique et déontologie',
-    description: '7 catégories - Les règles déontologiques, procédures et pratiques professionnelles'
-  },
-  {
     id: 2,
     name: 'Droit appliqué',
     description: '12 catégories - Procédures, droits des familles, contrats et plus'
+  },
+  {
+    id: 1,
+    name: 'Éthique et déontologie',
+    description: '7 catégories - Les règles déontologiques, procédures et pratiques professionnelles'
   }
 ];
 
